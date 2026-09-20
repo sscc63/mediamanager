be762c18802293df276bf168d08cd91d35721933
Scds64 <834344946@qq.com>
2026-09-20 17:41:58 +0800

    ui: 汉堡菜单透明度调至 0.67 并去掉外圈描边
    - 面板底色 0.72 -> 0.67
    - 删除 border: 1px solid rgba(255,255,255,.10)，圆角边缘改由毛玻璃与投影界定
    
    版本号 0.9.53 -> 0.9.54

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.9.53"
+var version = "0.9.54"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
